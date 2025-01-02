60191ff1753527b11dd8a13466e16b58075465aa
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-01-02 20:44:54 +0300

    feat: fetch product data from Coffee API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import Homepage from "./pages/Homepage/Homepage.jsx";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
       <Outlet />
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,3 +1,29 @@
+import { useState, useEffect } from "react";
+import classes from "./Products.module.css";
+
 export default function Header() {
-  return <div>THIS IS THE PRODUCTS PAGE</div>;
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fake-coffee-api.vercel.app/api?limit=9") //fetch 9 items from the fake coffee api
+      .then((res) => res.json())
+      .then((data) => setProductData(data)) //set the fetched items in the state
+      .catch((err) => console.log(err));
+  }, []);
+
+  return productData.length == 9 ? ( //checks if all items have been fetched
+    <div className={classes["product-grid"]}>
+      {" "}
+      {productData.map((product) => {
+        return (
+          <div className={classes["product-card"]} key={product.id}>
+            <img className={classes["product-image"]} src={product.image_url} />
+            <p className={classes["product-label"]}> {product.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
 }
